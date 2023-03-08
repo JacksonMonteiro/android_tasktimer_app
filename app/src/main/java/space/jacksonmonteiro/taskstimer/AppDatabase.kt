e69fc8a2d751fb6fddeb58ec133f878134d263e9
jacksonmonteiro <infor.jackson324@gmail.com>
2023-03-08 14:22:10 -0300

    Creating sql for task table and singleton app database class

--- app/src/main/java/space/jacksonmonteiro/taskstimer/AppDatabase.kt
+++ app/src/main/java/space/jacksonmonteiro/taskstimer/AppDatabase.kt
@@ -10,17 +10,36 @@ private const val TAG = "AppDatabase"
 private const val DATABASE_NAME = "TaskTimer.db"
 private const val DATABASE_VERSION = 1
 
-internal class AppDatabase constructor(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+internal class AppDatabase private constructor(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     init {
         Log.d(TAG, "AppDatabase: initialising")
     }
 
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase) {
+        Log.d(TAG, "onCreate: starts")
+        val sSQL = """CREAT TABLE ${TasksContract.TABLE_NAME} (
+            ${TasksContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL, 
+            ${TasksContract.Columns.TASK_NAME} TEXT NOT NULL,
+            ${TasksContract.Columns.TASK_DESCRIPTION} TEXT,
+            ${TasksContract.Columns.TASK_SORT_ORDER} INTEGER);
+        """.replaceIndent(" ")
+        Log.d(TAG, sSQL)
+        db.execSQL(sSQL)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("Not yet implemented")
     }
 
+    companion object : SingletonHolder<AppDatabase, Context>(::AppDatabase)
+
+    /*companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase = instance ?: synchronized(this) {
+            instance ?: AppDatabase(context).also {
+                instance = it
+            }
+        }
+    }*/
 }
